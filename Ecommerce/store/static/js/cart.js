605b2d186a492b3d3e1a20cc3d2ac3dabc581ed8
alishker <alinoorshukri7@gmail.com>
2020-05-12 21:33:50 +0300

    cart.js more javascript updates

--- Ecommerce/store/static/js/cart.js
+++ Ecommerce/store/static/js/cart.js
@@ -15,3 +15,23 @@ for (i = 0; i < updateBtns.length; i++) {
         }
     })
 }
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            location.reload()
+        });
+}
