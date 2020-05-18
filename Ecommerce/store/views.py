a0d443b25c41972841b793414e346495b900ad54
alishker <alinoorshukri7@gmail.com>
2020-05-18 07:02:19 +0300

    updateItemUrl and ProcessOrderUrl

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -8,6 +8,17 @@ from .models import *
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0,
+                 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/store.html', context)
@@ -19,11 +30,13 @@ def cart(request):
         order, created = Order.objects.get_or_create(
             customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
 
         # Create empty cart for now for non-logged in user
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
@@ -34,11 +47,13 @@ def checkout(request):
         order, created = Order.objects.get_or_create(
             customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
 
         # Create empty cart for now for non-logged in user
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
 
